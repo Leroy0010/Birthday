88c659d042c4e33edaad902d8b9e6b031aa81100
Leroy0010 <167775533+Leroy0010@users.noreply.github.com>
2024-05-06 23:45:24 +0000

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+var playing = false;
+var sound = new Audio("./01-Monk-Turner-Fascinoma-Its-Your-Birthday(chosic.com).mp3");
+
 $(".btn").on("click", function () {
-    var sound = new Audio("./01-Monk-Turner-Fascinoma-Its-Your-Birthday(chosic.com).mp3");
-    sound.play();
-    $('.btn').addClass("pressed");
-    setTimeout(function () {
-        $(".btn").removeClass("pressed");
-    }, 500);
+    $(this).toggleClass("down");
+    
+    if(!playing){
+        sound.play();
+        $('.btn').addClass("pressed");
+        setTimeout(function () {
+            $(".btn").removeClass("pressed");
+        }, 500);
+        $(".btn").text("Pause Song");
+        playing = true;
+    } 
+    else if(playing) {
+            sound.pause();
+            $(".btn").text("Play Song");
+            playing = false;
+  
+    }
 }
 );
